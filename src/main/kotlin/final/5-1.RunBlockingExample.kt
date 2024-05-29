6540de006c4e1875e0abdb2004cb64efefde424b
Incheol-Jung <bluesky60960@gmail.com>
2024-05-30 07:45:38 +0900

    불필요한 코드 제거

--- src/main/kotlin/final/5-1.RunBlockingExample.kt
+++ src/main/kotlin/final/5-1.RunBlockingExample.kt
@@ -8,24 +8,6 @@ import kotlin.time.measureTimedValue
  * @author Incheol.Jung
  * @since 2024. 05. 19.
  */
-class RunBlockingExample {
-    private suspend fun getNumbers(): List<Int> {
-        delay(1000)
-        return listOf(1, 2, 3, 4, 5)
-    }
-
-    fun calculate() {
-        val timedValue = measureTimedValue {
-            runBlocking {
-                val numbers = async { getNumbers() }
-                val numbers2 = async { getNumbers() }
-                numbers.await() + numbers2.await()
-            }
-        }
-        println(timedValue)
-    }
-}
-
 class DiscSaveRepository {
     suspend fun loadSave(name: String): String {
         val temp = withContext(Dispatchers.IO){
